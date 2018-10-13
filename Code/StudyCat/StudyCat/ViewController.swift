666884892ff214c0e9e4126f64c7bf066041af60
C_Martin <cmrt@Blackfyre.local>
2018-10-13 14:10:18 -0700

    test

---
+++ Code/StudyCat/StudyCat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StudyCat
+//
+//  Created by C_Martin on 10/13/18.
+//  Copyright © 2018 Kit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

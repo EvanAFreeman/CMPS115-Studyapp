2da456cf855a5a399d8c77775044229ab5b3d8b2
Camille Rogers <camillerogers215@gmail.com>
2018-10-17 12:34:00 -0700

    xcode updated project 10/17/18

--- Code/StudyCat/StudyCat/ViewController.swift
+++ Code/StudyCat/StudyCat/ViewController.swift
@@ -10,9 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var cat_in_box: UIImageView!
+    var box: UIImage!
+    var box1: UIImage!
+    var box2: UIImage!
+    var box3: UIImage!
+    var box4: UIImage!
+    var box5: UIImage!
+    
+    var images: [UIImage]!
+    
+    var animatedImage: UIImage!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        box1 = UIImage(named: "box_1")
+        box2 = UIImage(named: "box_2")
+        box3 = UIImage(named: "box_3")
+        box4 = UIImage(named: "box_4")
+        box5 = UIImage(named: "box_5")
+        
+        images = [box1, box2, box3, box4, box5]
+        
+   
+        animatedImage = UIImage.animatedImage(with: images, duration: 1.0)
+        
+        cat_in_box.image = animatedImage
+        
+  
     }
 
     override func didReceiveMemoryWarning() {

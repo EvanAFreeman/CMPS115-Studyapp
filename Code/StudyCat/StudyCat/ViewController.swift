222fa5bf1ddb68776692f682da811843ad3f04d3
C_Martin <cmrt@eduroam-169-233-174-30.ucsc.edu>
2018-11-02 12:36:46 -0700

    new branch with firebase images

--- Code/StudyCat/StudyCat/ViewController.swift
+++ Code/StudyCat/StudyCat/ViewController.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseStorage
+import FirebaseDatabase
 
 class ViewController: UIViewController {
 
@@ -19,7 +22,11 @@ class ViewController: UIViewController {
     var box4: UIImage!
     var box5: UIImage!
     
+    
+    
     var images: [UIImage]!
+    let storage = Storage.storage().reference()
+    let database = Database.database().reference()
     
     var animatedImage: UIImage!
     
@@ -27,14 +34,39 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        //let bundleIdentifier =  Bundle.main.bundleIdentifier
+        //print(bundleIdentifier!)
+        let metaData = StorageMetadata()
+        metaData.contentType = "image/jpeg"
         
+        images = loadCatBox()
+        
+        /*
         box1 = UIImage(named: "box_1")
         box2 = UIImage(named: "box_2")
         box3 = UIImage(named: "box_3")
         box4 = UIImage(named: "box_4")
         box5 = UIImage(named: "box_5")
+ 
         
         images = [box1, box2, box3, box4, box5]
+ 
+    
+        for (index, image) in images.enumerated() {
+            //print(String(format: "image number %d\n", index))
+            let temp = storage.child(String(format: "cat_pictures/box_%d", index+1))
+            
+            temp.putData(UIImageJPEGRepresentation(image, 0.8)!, metadata: metaData) { (data, error) in
+                if error == nil {
+                    print("upload successful")
+                } else {
+                    print("error")
+                }
+            }
+            
+        }
+         */
+ 
         
    
         animatedImage = UIImage.animatedImage(with: images, duration: 1.0)
@@ -42,7 +74,42 @@ class ViewController: UIViewController {
             cat_in_box.image = animatedImage
         }
     }
+    
+    /*
+    func uploadImage(filename: String) {
+        let temp = storage.child(String(format: "cat_pictures/box_%d", index))
+        temp.putData(UIImageJPEGRepresentation(image, 0.8)!, metadata: metaData) { (data, error) in
+            if error == nil {
+                print("upload successful")
+            } else {
+                print("error")
+            }
+        }
 
+    }
+     */
+
+    func loadCatBox() -> Array<UIImage>{
+        
+        var images = Array<UIImage>()
+        
+        for i in 1...5 {
+            let temp = storage.child(String(format: "cat_pictures/box_%d", i))
+            temp.getData(maxSize: 1*1000*1000) { (data, error) in
+                if error == nil {
+                    print(String(format: "pic %d retreived successfully\n", i))
+                } else {
+                    print(String(format: "there was an error loading the cat %d\n", i))
+                }
+            }
+            
+            images.append(UIImage(named: String(format: "box_%d", i))!)
+        }
+        return images
+        
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
